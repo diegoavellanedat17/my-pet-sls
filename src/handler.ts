4b1ef12fcdb63a398fdafc4cbbb1e6c9c172fe27
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-07-18 23:17:25 -0500

    Started workflow for ci-cd the lambda

--- src/handler.ts
+++ src/handler.ts
@@ -4,7 +4,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/hello', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello World' });
+  console.log('received:', { req, res });
+  res.status(200).json({ message: 'Hello Test' });
 });
 
 export const handler = serverless(app);
